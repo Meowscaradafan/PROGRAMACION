033f8e221fe07ba4c71cd8bc9a0e4a28969cfd40
Meowscaradafan <163231758+Meowscaradafan@users.noreply.github.com>
2024-06-19 20:46:20 -0400

    cambio3

--- buckshot.py
+++ buckshot.py
@@ -1,128 +1,94 @@
-import random
+import random    #para que funcionen los random.choice
+
+def coinflipapostador():
+    if random.choice([1,2]) == 1:
+        print("el apostador te dispara")
+    elif random.choice([1,2]) == 2:
+        print("el apostador se dispara")    #este es el 50/50 de el apostador, si decide disparar al jugador o a el
+
+def vidarandom():
+    cantidad = [2,3,4]
+    eleccion = random.choice(cantidad)
+    return eleccion    #aqui definimos la vida de los jugadores
+
+vida2jugadores = vidarandom()
+vidajugador = vida2jugadores
+vidaapostador = vida2jugadores #aqui le asignamos la vida a ambos jugadores
+
+def balasrandom():
+    cantidadbalas = [2,3,4,5]
+    eleccionbalas = random.choice(cantidadbalas)
+    return eleccionbalas  #aqui definimos la cantidad de balas
+
+balaslistas = balasrandom() #definimos la cantidad de balas a una variable
+
+def cargas():
+    if balaslistas == 2:
+        reales = 1
+    elif balaslistas == 3:
+        reales = random.choice([1,2])
+    elif balaslistas == 4:
+        reales = 2
+    elif balaslistas == 5:
+        reales = random.choice([2,3])
+        return reales   #aqui definimos la cantidad de balas reales entre la cantidad de balas definida anteriormente
+cargass = cargas()
 
 
-def coinflip():
-    if random.choice[(1,2)] == "1":
-        print("el apostador te dispara ")
+def cargas2():
+    balas = list(range(1, balaslistas + 1))
+    cargadas = random.sample(balas, cargass)
+    balascargador = [(bala, "real" if bala in cargadas else "falsa") for bala in balas]
+    return balascargador  #aqui tendremos la lista de balas reales y falsas en orden de forma aleatoria en el sistema
 
-    elif random.choice[(1,2)] == "2":
-        print("el apostador se dispara ")
+balascargadorordenadas = cargas2()  #definimos la lista de balas en una variable
+print(balascargadorordenadas)
+#aca tendremos un codigo para los 3 items del juego
 
+bebida = "bebida"  #la bebida hara que al que este de turno pueda descargar una bala de la escopeta
+lupa = "lupa"  #la lupa hara que el que este de turno sepa que bala es la que biene en el siguiente disparo
+cigarros = "cigarros"  #los cigarros hara que el que este de turno se regenere 1 de vida
 
+cajaitems = [bebida, lupa, cigarros]  #esta es la lista de items
 
+inventariojugador = random.choice([bebida, lupa, cigarros])
+inventarioapostador = random.choice([bebida, lupa, cigarros]) #estas variables son para que al principio de la ronda se les asigne 1 item a cada jugador
 
-print("Bienvenido a buckshot roulette ")
-usuario = input("Cual es tu nombre? ")
+apostadoreleccionitem = random.choice([inventarioapostador]) #aca el apostador escojera el item que tiene en su inventario para usarlo
 
-while True:
-    print("1. Para empezar ")
-    print("2. Para terminar ")
-    Q1 = input("Que deseas hacer? (1-2)")
+disparosjugador = []
+disparosapostador = [] #estas variables son para que se guarden los disparos de cada jugador en una lista
 
-    if Q1 == "1":
-        print("vamos a jugar, las reglas son simples. Tienes que ganarle al dealer que esta del otro lado, con una escopeta con numero de balas aleatoria y con cargas aleatorias. Hay dos tipos de balas, la real y la descargada. Se te asignara items con diferentes usos para ganarle al dealer. Buena suerte")
-        def vidaR1():
-            cantidad = [2,3,4]
-            eleccion = random.choice(cantidad)
-            return eleccion
+disparosronda = [] #esta variable hara que la ronda acabe cuando se llegue a la misma cantidad que la variable balaslistas
 
-        vida2players = vidaR1()
-        vidajugador = vida2players
-        vidaapostador = vida2players
-
-        def balasR1():
-            cantidadbalas = [3,4,5]
-            eleccionronda = random.choice(cantidadbalas)
-            return eleccionronda
-
-        balasRo1 = balasR1()
-        def cargasR1():
-            if balasRo1 == 3:
-               balasreales = random.choice([1,2])
-            elif balasRo1 == 4:
-                balasreales = random.choice([2])
-            elif balasRo1 == 5:
-                balasreales = random.choice([2,3])
-                return balasreales
+ #nota
+ #hacer un codigo para que el apostador sepa siempre cual es la ultima bala en el cargador
+ #hacer un while para la ronda y break para cuando esta acabe cuando la vida de uno de los jugadores lleguen a 0
+ #hacer un codigo para que cada vez que se dispare uno mismo, ya sea el jugador o apostador, este pueda disparar otra carga
 
 
-            balas = list(range(1, balasRo1 + 1))
-            reales = random.sample(balas, balasreales)
-            balascargador = [(bala, "real" if bala in reales else "falsa") for bala in balas]
-            return balascargador
 
-        balascargador = cargasR1()
 
-          
+
+falsas = cargass - balaslistas
+
+
+print("Bienvenido a buckshot roulette\n")
+
+usuario = input("Ingresa tu nombre: ")
+
+print(usuario, "tu vida es ",vidajugador,"y la del apostador", vidaapostador)
+
+print("\nprimera ronda, hay", balaslistas,"balas en el cargador")
+print("las balas son las siguientes: ",cargass,"cargadas",falsas,"descargadas" )
+
+ronda1 = input("\n1)Disparar al apostador\n2)Dispararte\n3)Usar item")
+if ronda1 == "1":
+    print("Disparas al apostador\n")
+    if balascargadorordenadas[0] == "1. real":
+        vidaapostador = vidaapostador - 1
+        print("bala real")
         
-        while True:
-
-            print(f"Tu vida es {vidajugador} y la vida de el apostador es {vidaapostador}")
-
-            print("\nPrimera ronda, la ronda acaba cuando alguno de los 2 pierdan toda su vida\n")
-
-            bebida = "bebida"
-            lupa = "lupa"
-            cigarros = "cigarros"
-
-            cajaitems = [bebida, lupa, cigarros,]
-            inventariojugador = random.choice([bebida, lupa, cigarros])
-            inventarioapostador = random.choice([bebida, lupa, cigarros])
-     #ordenar balascargador en una lista para que el sistema sepa el
-     # orden de las balas para que pueda determinar si es real o falsa
-            cargadorsistema = [balascargador]
-            disparosjugador = []
-            disparosapostador = []
-            disparosrondas = []
-            apostadoreleccionitems = random.choice([inventarioapostador])
-
-            def coinflipapostador():
-              if random.choice[(1,2)] == "1":
-                 print("el apostador te dispara ")
-                 if cargadorsistema[0][1] == "real":
-                     print("bala real")
-                     vidajugador = vidajugador - 1
-                 elif cargadorsistema[0][1] == "falsa":
-                     print("bala falsa")
-              elif random.choice[(1,2)] == "2":
-                 print("el apostador se dispara ")
-                 if cargadorsistema[0][1] == "real":
-                     print("bala real")
-                     vidaapostador = vidaapostador - 1
-                 elif cargadorsistema[0][1] == "falsa":
-                     print("bala falsa")
-
-              return vidajugador, vidaapostador
-
-            print(inventariojugador)
-            print(inventarioapostador)
-            print(cargadorsistema)
-
-            print("se han cargado",balasRo1,"balas\n")
         
-            decision1 = input("1) disparar al apostador\n2) dispararte\n3) mostrar items ")
-            
-            if decision1 == "1":
-                disparosjugador.append(balascargador[0][0])
-                if disparosjugador[0] == "real":
-                    vidaapostador = vidaapostador - 1
-                    print("bala real")
-                    print("tu vida",vidajugador,"apostador",vidaapostador)
-                elif disparosjugador[0] == "falsa":
-                    print("bala falsa")
-            if decision1 == "2":
-                disparosjugador.append(balascargador[0][0])
-                if disparosjugador[0] == "real":
-                    vidajugador = vidajugador - 1
-                    print("bala real")
-                    print("tu vida",vidajugador,"apostador",vidaapostador)
-                elif disparosjugador[0] == "falsa":
-                    print("bala falsa")
-                    
-                    print("tu vida:",vidajugador,"\nvida apostador:",vidaapostador)
-                    break
-
-             
 
 
-            
